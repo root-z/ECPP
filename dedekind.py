15c4ee71a6cd436e16f36d7bd4c99225867bef85
zane <zanelin1990@gmail.com>
2015-12-02 16:42:09 -0500

    hilbert

--- dedekind.py
+++ dedekind.py
@@ -15,8 +15,9 @@ def dedekind(tau, floatpre):
     """
     a = 2 * mpmath.pi / mpmath.mpf(24)
     b = mpmath.exp(mpmath.mpc(0, a))
+    print 'b=', b
     # b = e^(2pi*i/24)
-    p = 1
+    p = 1   # What is this for?
     m = 0
     # What the hell is happening in this for loop
     '''Removing the integer part of the tau.real '''
@@ -26,14 +27,18 @@ def dedekind(tau, floatpre):
             tau -= n
             p *= b**n
         m = tau.real*tau.real + tau.imag*tau.imag
+        # keeping the absolute of tau less than 1
         if m <= 0.999:
             ro = mpmath.sqrt(mpmath.power(tau, -1)*1j)
+            # ro = sqrt((tau^-1)*i)
             if ro.real < 0:
                 ro = -ro
             p = p*ro
             tau = (-p.real + p.imag*1j) / m
-    print('tau=', tau, '\n p =', p)
+    print 'tau=', tau, '\n p =', p
+
     q1 = mpmath.exp(a*tau*1j)
+    print 'q1=', q1
     # q1 = e^(2pi*tau*i/24)
     q = q1**24
     # q = e^(2pi*tau*i)
@@ -53,7 +58,9 @@ def dedekind(tau, floatpre):
         # qs = -q^2, q^7
         s += t + qs
         # s = 1 - q - q^2, 1-q-q^2+q^5+q^7
+
     return p*q1*s
+    # Compare to wolfram alpha the result is correct.
 
 if __name__=='__main__':
-    print(dedekind(0.2 + 0.1j, 8))
+    print dedekind(1+ + 2j, 8)
