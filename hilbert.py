5dc317c3f0f266fb035a06ef06b8cfd4b8fd8a5a
zane <zanelin1990@gmail.com>
2015-12-12 00:28:15 +0800

    dedekind comments added. I think I understand most of the algorithm now.

--- hilbert.py
+++ hilbert.py
@@ -4,22 +4,30 @@ Compute Hilbert Class Polynomials
 from mpmath import floor, sqrt, power
 from dedekind import dedekind
 import mpmath
-import nzmath.ecpp
+import nzmath.quad as quad
 
 
 def solve(d):
     # initialize
     t = [1]
     b = d % 2
-    r = floor(sqrt((-d)/3))
+    r = floor(sqrt((-d)/3))-1
     h = 0
-    red = {}
+    red = set()
+
+    h1, reduced_forms = quad.class_group(d)
+    print h1
+    harmonic_sum = sum(1/mpmath.mpf(form[0]) for form in reduced_forms)
+    floatpre = floor(mpmath.pi*mpmath.sqrt(-d)*harmonic_sum / mpmath.log(10)+0.5) + 10
+    mpmath.mp.dps = floatpre
+
+
 
     # outer loop
-    while (b <= r) :
+    while b <= r:
         m = (b*b - d) / 4
-        a = 0
-        while a * a <= m:
+        m_sqrt = int(floor(sqrt(m)))
+        for a in range(1, m_sqrt+1):
             if m % a != 0:
                 continue
             c = m/a
@@ -27,16 +35,33 @@ def solve(d):
                 continue
             # optional polynomial setup
             tau = (-b + 1j * sqrt(-d)) / (2*a)
-            f = power(dedekind(2 * tau) / dedekind(tau), 24)
+            f = power(dedekind(2 * tau, floatpre) / dedekind(tau, floatpre), 24)
             j = power((256 * f + 1), 3) / f
 
             if b==a or c==a or b==0:
-                t = polynomialMul(t, [-1j, 1])
+                # T = T * (X-j)
+                t = polynomial_mul(t, [-j, 1])
+                h += 1
+                red.add((a, b, c))
+            else:
+                poly = [j.real * j.real + j.imag * j.imag, -2 * j.real, 1]
+                t = polynomial_mul(t, poly)
+                h += 2
+                red.add((a, b, c))
+                red.add((a, -b, c))
+        b += 1
+
+    return h, [floor(mpmath.re(p) + 0.5) for p in t], red
+
 
 def delta(q):
     return q
 
-def polynomialMul(p1, p2):
+
+def polynomial_mul(p1, p2):
+    '''
+    Used to Compute T = T * (X-j)
+    '''
     if len(p1)==0 or len(p2)==0:
         raise ValueError('Polynomial Array empty.')
     m = [0]* (len(p1) + len(p2) -1)
@@ -52,4 +77,4 @@ if __name__ == '__main__':
     print power(1j, 2)
     print nzmath.ecpp.hilbert(-15)
     '''
-    print polynomialMul([0,1], [-1j, 1])
+    print solve(-4)
